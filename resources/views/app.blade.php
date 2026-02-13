501cdabdbc533cfbda118305a361a96e0fec7d66
Fauzan Azmi <fauzan.azmi@suryaenergi.com>
2026-02-13 16:32:24 +0700

    fix: update favicon configuration to use SVG only with cache-busting
    - Remove favicon.ico to avoid old cached icon
    - Add cache-busting parameter (?v=2) to favicon.svg
    - Simplify favicon links to only use SVG (modern standard)
    - Users should do hard refresh (Ctrl+Shift+R) to see new icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,10 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="alternate icon" href="/favicon.ico" sizes="any">
-        <link rel="apple-touch-icon" href="/favicon.svg">
+        <!-- SatsetUI Favicon -->
+        <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/favicon.svg?v=2">
+        <link rel="shortcut icon" href="/favicon.svg?v=2">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
